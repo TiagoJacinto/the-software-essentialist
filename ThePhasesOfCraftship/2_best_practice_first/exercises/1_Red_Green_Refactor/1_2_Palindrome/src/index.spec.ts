c4860ca817bec7b65c2b7338e9a8c47de600cbcd
Tiago <tiagojacintodev@gmail.com>
2024-03-18 11:40:51 +0000

    [refactor]: surround tests with describe blocks

--- ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/exercises/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -7,45 +7,53 @@ describe('palindrome checker', () => {
     palindromeChecker = new PalindromeChecker();
   });
 
-  it.each(['mom', 'wow'])(
-    'should be able to verify that the word "%s" is a valid palindrome',
-    (word) => {
+  describe('words', () => {
+    it.each(['mom', 'wow'])('knows that "%s" is a valid palindrome', (word) => {
       const result = palindromeChecker.isAPalindrome(word);
 
       expect(result).toBeTruthy();
-    }
-  );
+    });
 
-  it.each(['Mom', 'DAD', 'WoW', 'MoM', 'xMomx'])(
-    'should be able to verify that the word "%s" with is a valid palindrome even if casing is off',
-    (word) => {
+    it.each(['bill', 'library', 'book'])(
+      'knows that "%s" is an invalid palindrome',
+      (word) => {
+        const result = palindromeChecker.isAPalindrome(word);
+
+        expect(result).toBeFalsy();
+      }
+    );
+  });
+
+  describe('casing is off', () => {
+    it.each([
+      'Mom',
+      'DAD',
+      'WoW',
+      'MoM',
+      'xMomx',
+      'NEVER Odd oR Even',
+      'Was It A RAT I SaW',
+    ])('knows that "%s" is a valid palindrome', (word) => {
       const result = palindromeChecker.isAPalindrome(word);
 
       expect(result).toBeTruthy();
-    }
-  );
+    });
+  });
 
-  it.each(['bill', 'library', 'book'])(
-    'should be able to verify that the word "%s" is an invalid palindrome',
-    (word) => {
-      const result = palindromeChecker.isAPalindrome(word);
+  describe('phrases', () => {
+    it.each(['Was It A Rat I Saw', 'Never Odd or Even', '1Never Odd or Even1'])(
+      'knows that "%s" is a valid palindrome',
+      (phrase) => {
+        const result = palindromeChecker.isAPalindrome(phrase);
 
-      expect(result).toBeFalsy();
-    }
-  );
+        expect(result).toBeTruthy();
+      }
+    );
 
-  it.each(['Was It A Rat I Saw', 'Never Odd or Even', '1Never Odd or Even1'])(
-    'should be able to verify that the phrase "%s" is a valid palindrome',
-    (phrase) => {
+    it('knows that "Never Odd or Even1" is an invalid palindrome', () => {
+      const phrase = 'Never Odd or Even1';
       const result = palindromeChecker.isAPalindrome(phrase);
-
-      expect(result).toBeTruthy();
-    }
-  );
-
-  it('should be able to verify that the phrase "Never Odd or Even1" is an invalid palindrome', () => {
-    const phrase = 'Never Odd or Even1';
-    const result = palindromeChecker.isAPalindrome(phrase);
-    expect(result).toBeFalsy();
+      expect(result).toBeFalsy();
+    });
   });
 });
